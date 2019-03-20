54d8ff5a44a618034cfa18484cdb1664ca44955b
moad <moadrehhali@gmail.com>
2019-03-20 13:10:48 +0100

    ajout du navbar et side bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   showHideSideBar :boolean = false;
   title = 'eauPotable';
+
+    onShowSideBarChange(onShowHideSideBar){
+    this.showHideSideBar = onShowHideSideBar;
+  }
 }
